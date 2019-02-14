f133815dd63e9ae5174f6e8db672e9037564bbd0
x <2509344578@qq.com>
2019-02-14 18:29:52 +0800

    搜索页

--- src/api/modules/search.js
+++ src/api/modules/search.js
@@ -3,3 +3,7 @@ const { getFn } = require('../request');
 exports.search = function(queryParams) {
     return getFn(queryParams);
 };
+
+exports.getHotKey = function(queryParams) {
+    return getFn(queryParams);
+};
